af614d694ac51a0017ff1dedc8dc7bf9c3b6858f
Hyewon Seo <Hyewooon826@gmail.com>
2024-09-30 01:44:48 +0900

    Add files via upload

---
+++ Tutorial_2/src/week2/Ex_1.java
@@ -0,0 +1,8 @@
+package week2;
+
+public class Ex_1 {
+	public static void main (String[] args) {
+		System.out.println("안녕하세요 oo학과에 다니고 있습니다.");
+		System.out.println("안녕하세요 제 이름은 ooo입니다.");
+	}
+}
---
+++ Tutorial_2/src/week2/Ex_2.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class Ex_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요 oo학과에 다니고 있습니다.");
+		System.out.print("안녕하세요 제 이름은 ooo입니다.");
+	}
+
+}
